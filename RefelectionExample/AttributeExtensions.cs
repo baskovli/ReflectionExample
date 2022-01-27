a31c21a3c98780eaa11957eafe84664e5edaeca8
MelAdo <melikalai232@gmail.com>
2022-01-27 11:41:19 +0200

    added solution

---
+++ RefelectionExample/AttributeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefelectionExample
+{
+    public static class AttributeExtensions
+    {
+        [AttributeUsage(AttributeTargets.Property)]
+        public class UseHeaderAttribute : Attribute
+        {
+
+        }
+
+        [AttributeUsage(AttributeTargets.Property)]
+        public class UseAsLineAttribute : Attribute
+        {
+
+        }
+        [AttributeUsage(AttributeTargets.Property)]
+        public class DoubleLengthAttribute : Attribute
+        {
+            public int Length;
+            public int AfteDot;
+
+            public DoubleLengthAttribute(int length, int afteDot)
+            {
+                Length = length;
+                AfteDot = afteDot;
+            }
+        }
+    }
+}
---
+++ RefelectionExample/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using static RefelectionExample.AttributeExtensions;
+
+namespace RefelectionExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = new List<ReflectionClass>
+            {
+                new ReflectionClass { Id  = 1, Name = "avb", Price = 61.23345 },
+                new ReflectionClass { Id  = 2, Price = 3.323234 },
+                new ReflectionClass { Id  = 3, Name = "rew", Price = 49.57553 },
+            };
+
+            var classType = new ReflectionClass();
+
+            Type myType = classType.GetType();
+
+            var props = new List<PropertyInfo>(myType.GetProperties());
+
+            List<PropertyInfoModel> propertyListInuse = new();
+
+            props.ForEach((item) =>
+            {
+                var existsHeaderAttribute = item.GetCustomAttributes(false).OfType<UseHeaderAttribute>().Any();
+                if (existsHeaderAttribute)
+                {
+                    propertyListInuse.Add(GetAttributeByType(item));
+                }
+            });
+
+            StringBuilder str = new StringBuilder();
+            string seperator = ";";
+            foreach (var item in data)
+            {
+                string val = string.Empty;
+
+                foreach (var prop in propertyListInuse)
+                {
+                    var value = GetValueByTypeAndAttribute(prop, prop.PropertyInfo.GetValue(item, null));
+                    val += $"{value}{seperator}";
+                }
+                val = val.Remove(val.Length - seperator.Length);
+
+                str.AppendLine(val);
+            }
+
+            Console.WriteLine(str);
+        }
+
+        public static PropertyInfoModel GetAttributeByType(PropertyInfo property)
+        {
+            var model = new PropertyInfoModel
+            {
+                PropertyInfo = property,
+            };
+
+            switch (property.PropertyType.Name)
+            {
+                case nameof(String):
+                    var maxLengthAttr = (MaxLengthAttribute)property.GetCustomAttribute(typeof(MaxLengthAttribute));
+                    model.FirstNum = maxLengthAttr?.Length;
+                    break;
+                case nameof(Int64):
+                case nameof(Int32):
+                case nameof(Int16):
+                    var rangeAttr = (RangeAttribute)property.GetCustomAttribute(typeof(RangeAttribute));
+                    if (rangeAttr != null)
+                    {
+                        model.FirstNum = (int)rangeAttr.Minimum;
+                        model.SecondNum = (int)rangeAttr.Maximum;
+                    }
+                    break;
+                case nameof(Decimal):
+                case nameof(Double):
+                    var doubleAttr = (DoubleLengthAttribute)property.GetCustomAttribute(typeof(DoubleLengthAttribute));
+                    if (doubleAttr != null)
+                    {
+                        model.FirstNum = doubleAttr.Length;
+                        model.SecondNum = doubleAttr.AfteDot;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return model;
+        }
+
+        public static string GetValueByTypeAndAttribute(PropertyInfoModel propertyData, object value)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            switch (propertyData.PropertyInfo.PropertyType.Name)
+            {
+                case nameof(String):
+                    var strValue = value.ToString();
+                    if (propertyData.FirstNum.HasValue)
+                    {
+                        if (strValue.Length > propertyData.FirstNum)
+                        {
+                            strValue = strValue.Substring(0, propertyData.FirstNum.Value);
+                        }
+                    }
+                    return strValue;
+                case nameof(Int64):
+                case nameof(Int32):
+                case nameof(Int16):
+                    return value.ToString();// Have to think about. We cannot change int values...
+                case nameof(Decimal):
+                case nameof(Double):
+                    var valueDouble = (double)value;
+                    if (propertyData.FirstNum.HasValue && propertyData.SecondNum.HasValue)
+                    {
+                        return valueDouble.ToString($"F{propertyData.SecondNum}", CultureInfo.InvariantCulture).Replace(",", ".");
+
+                    }
+                    return valueDouble.ToString("F", CultureInfo.InvariantCulture).Replace(",", ".");
+                case nameof(DateTime):
+                    return ((DateTime)value).ToString("yyyyMMdd");
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+
+    public class ReflectionClass
+    {
+        [UseHeader]
+        [Range(1, 100)]
+        public int Id { get; set; }
+        [UseHeader, DoubleLength(5, 2)]
+        public double Price { get; set; }
+        [UseHeader]
+        [MaxLength(6)]
+        public string Name { get; set; }
+    }
+
+    public class PropertyInfoModel
+    {
+        public PropertyInfo PropertyInfo { get; set; }
+        public int? FirstNum { get; set; }
+        public int? SecondNum { get; set; }
+    }
+}
